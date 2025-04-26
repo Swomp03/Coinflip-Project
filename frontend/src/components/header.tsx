8f861758fba4522d6ac68b02bd7beaa40b28f2e4
Swomp03 <robbie.yx.l@gmail.com>
2025-04-26 17:49:04 -0400

    Styling changes

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -5,7 +5,7 @@ const Header = (props: any) => {
 
     return (
         <div className='header'>
-            <h2 id='flipDate'>Last Coin Flip Date:<br /> {props.lastFlippedDate}</h2>
+            {/* <h2 id='flipDate'>Last Flip:<br />{props.lastFlippedDate} {props.lastFlippedDate} GMT</h2> */}
             <h1 id='title'>Coin Flipper</h1>
             <h2 id='totalFlippedCoins'>Total Flipped Coins: <br /> {props.totalFlips}</h2>
         </div>
